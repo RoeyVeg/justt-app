4f6f01b606e02171cd67d188a5cc6a0a15731323
Roey Wegerhoff <iroey.veg@gmail.com>
2021-12-18 10:48:44 +0200

    added refresh button

--- src/components/TransactionsTable.jsx
+++ src/components/TransactionsTable.jsx
@@ -5,13 +5,25 @@ const TransactionsTable = (props) => {
   useEffect(() => {
     (async () => {
       const trans = await props.store.fetchAllTransactions();
-      console.log({ trans });
       setTransactions(trans.data);
     })();
   }, []);
+  const refresh = async () => {
+    const trans = await props.store.fetchAllTransactions();
+    setTransactions(trans.data);
+  };
   return (
     <div className="row">
       <div className="h3 m-3">Transactions</div>
+      <div className="col-4">
+        <button
+          className="btn btn-primary"
+          type="button"
+          onClick={() => refresh()}
+        >
+          Refresh
+        </button>
+      </div>
       <hr />
       <div class="row ms-3 justify-content-start">
         {transactions.map((trans, index) => (
